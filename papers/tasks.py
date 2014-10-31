0e8eb6dac9e1843ec31d5c4c8b63c47447ef0579
Antonin Delpeuch <antonin@delpeuch.eu>
2014-10-31 12:31:46 +0000

    A bit of refactoring in papers/tasks.py

--- papers/tasks.py
+++ papers/tasks.py
@@ -22,6 +22,47 @@ from papers.crossref import fetch_papers_from_crossref_by_researcher_name, conve
 
 logger = get_task_logger(__name__)
 
+def process_records(listRecords, source):
+    count = 0
+    saved = 0
+    for record in listRecords:
+        # Update task status
+        if count % 100 == 0:
+        source.status = '%d records processed, %d records saved' % (count,saved)
+        source.save()
+        count += 1
+
+        metadata = record[1]._map
+        authors = get_oai_authors(metadata)
+
+        # Filter the record
+        if all(not elem.is_known for elem in authors):
+        continue
+        if not 'title' in metadata or metadata['title'] == []:
+        continue
+
+        # Find the DOI, if any
+        doi = None
+        for identifier in metadata['identifier']:
+        if not doi:
+            doi = to_doi(identifier)
+
+        pubdate = find_earliest_oai_date(record)
+        year = None
+        if pubdate:
+        year = pubdate.year
+        else:
+        print "No publication date, skipping"
+        continue
+
+        logger.info('Saving record %s' % record[0].identifier())
+        paper = get_or_create_paper(metadata['title'][0], authors, year, doi) # TODO replace with real pubdate
+        # Save the record
+        add_oai_record(record, source, paper)
+        saved += 1
+   return (count,saved)
+
+
 @shared_task
 def fetch_items_from_oai_source(pk):
     source = OaiSource.objects.get(pk=pk) # this is safe because the PK is checked by the view
@@ -34,7 +75,6 @@ def fetch_items_from_oai_source(pk):
 
         start_date = source.last_update.replace(tzinfo=None)
         restrict_set = source.restrict_set
-        print "Getting list of records..."
         try:
             if restrict_set:
                 listRecords = client.listRecords(metadataPrefix='oai_dc', from_= start_date, set=restrict_set)
@@ -43,46 +83,9 @@ def fetch_items_from_oai_source(pk):
             # TODO make it less naive, for instance convert to UTC beforehand
         except NoRecordsMatchError:
             listRecords = []
-        print "Got list of records"
+        
+        (count,saved) = process_records(listRecords, source)
 
-        count = 0
-        saved = 0
-        for record in listRecords:
-            # Update task status
-            if count % 100 == 0:
-                source.status = '%d records processed, %d records saved' % (count,saved)
-                source.save()
-            count += 1
-
-            metadata = record[1]._map
-            authors = get_oai_authors(metadata)
-
-            # Filter the record
-            if all(not elem.is_known for elem in authors):
-                continue
-            if not 'title' in metadata or metadata['title'] == []:
-                continue
-
-            # Find the DOI, if any
-            doi = None
-            for identifier in metadata['identifier']:
-                if not doi:
-                    doi = to_doi(identifier)
-
-            pubdate = find_earliest_oai_date(record)
-            year = None
-            if pubdate:
-                year = pubdate.year
-            else:
-                print "No publication date, skipping"
-                continue
-
-            logger.info('Saving record %s' % record[0].identifier())
-            paper = get_or_create_paper(metadata['title'][0], authors, year, doi) # TODO replace with real pubdate
-            # Save the record
-            add_oai_record(record, source, paper)
-            saved += 1
-       
         # Save the current date
         source.status = 'OK, %d records fetched.' % count
         source.last_update = timezone.now()
@@ -91,7 +94,6 @@ def fetch_items_from_oai_source(pk):
         source.status = 'ERROR: '+unicode(e)
         source.save()
         raise
-
 
 @shared_task
 def fetch_dois_for_researcher(pk):
