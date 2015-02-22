9e537ebc4f46ca021742cbb9d06c2caf245ac558
Thomas E. Bourgeat <thomas.bourgeat@ens.fr>
2015-02-22 12:40:38 +0100

    Toward a one click deposit

---
+++ papers/submitPaper.py
@@ -0,0 +1,20 @@
+from sword2 import *
+
+#http://api-test.archives-ouvertes.fr/sword/
+#test-ws test
+
+c = Connection("http://api-preprod.archives-ouvertes.fr/sword/servicedocument",		user_name="bthom", user_pass="dissemin")
+
+#c=Connection("http://127.0.0.1:1234/sd-uri", user_name="sword", user_pass="sword")
+c.get_service_document()
+print type(c)
+
+collection = c.workspaces[0][1][0]
+print collection
+with open("art.xml","r") as data:
+	receipt = c.create(col_iri=collection.href,
+									packaging = "http://purl.org/net/sword-types/AOfr",
+							  	metadata_entry = data,
+									#filename = "art.xml",
+									mimetype = "text/xml"
+									)
