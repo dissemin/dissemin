05ffb258f4a08f1ed20927f2b8872d2f3c85d117
Thomas E. Bourgeat <thomas.bourgeat@ens.fr>
2015-02-23 01:27:14 +0100

    Zip and Hal

--- sword/submitPaper.py
+++ sword/submitPaper.py
@@ -1,5 +1,6 @@
 import httplib
-
+from zipfile import *
+import StringIO
 
 ## simple wrapper function to encode the username & pass
 def encodeUserData(user, password):
@@ -12,21 +13,59 @@ conn = httplib.HTTPConnection("api-preprod.archives-ouvertes.fr")
 #httplib.HTTPConnection.debuglevel = 1 #Uncomment to debug mode
 
 
-def CreateMetadataHal(fl):  #Homemade sword protocol 
-	with open(fl,"r") as data:
-		strData = data.read()#.replace("\n","").replace("\t","").replace("\r","")
-		conn.putrequest("POST", "/sword/hal/", True,True)
-		conn.putheader("Authorization",encodeUserData(u,p))
-		conn.putheader("Host","api-preprod.archives-ouvertes.fr")
-#		conn.putheader("Accept","*/*")  #useless
-		conn.putheader("X-Packaging","http://purl.org/net/sword-types/AOfr")
-		conn.putheader("Content-Type","text/xml")
-		conn.putheader("Content-Length",len(strData))
-		conn.endheaders()
-		conn.send(strData)
-		r1 = conn.getresponse()
-		print r1.read()
+#TODO add specific headers for export-toarxiv and so one
+#DOC in french on api-preprod.archives-ouvertes.fr
+
+#TODO Abstract on the connection
 
 
-CreatemetadataHal("art.xml")
+def MetadataHal(strData):  #Homemade sword protocol 
+#TODO refactorize the code, fold on the headers
+	conn.putrequest("POST", "/sword/hal/", True,True)
+	conn.putheader("Authorization",encodeUserData(u,p))
+	conn.putheader("Host","api-preprod.archives-ouvertes.fr")
+	conn.putheader("X-Packaging","http://purl.org/net/sword-types/AOfr")
+	conn.putheader("Content-Type","text/xml")
+	conn.putheader("Content-Length",len(strData))
+	conn.endheaders()
+	conn.send(strData)
+	r1 = conn.getresponse()
+	print r1.read()
+
+
+def CreateZipFromPdfAndMetadata(pdf,metadata):
+	s = StringIO.StringIO()
+	with ZipFile(s,'w') as myZip:
+		myZip.writestr("article.pdf",pdf) 
+		myZip.writestr("meta.xml",metadata)
+		myZip.close()
+	return s.getvalue()
+
+
+def FullHal(pdf,metadata):
+	strData = CreateZipFromPdfAndMetadata(pdf,metadata)
+	conn.putrequest("POST", "/sword/hal/", True,True)
+	conn.putheader("Authorization",encodeUserData(u,p))
+	conn.putheader("Host","api-preprod.archives-ouvertes.fr")
+	conn.putheader("X-Packaging","http://purl.org/net/sword-types/AOfr")
+	conn.putheader("Content-Type","application/zip")
+	conn.putheader("Content-Disposition","attachment; filename=meta.xml")
+	conn.putheader("Content-Length",len(strData))
+	conn.endheaders()
+	conn.send(strData)
+	r1 = conn.getresponse()
+	print r1.read()
+
+with open("meta.xml","r") as xml:
+	with open("article.pdf","r") as art:
+		FullHal(xml.read(),art.read())
+
+#TODO parse the XML to search "<edition>" then add the src
+#TODO Full SSL. Right now user/pass are not crypted.
+#TODO new version file on HAL
+#TODO modification of metadata on HAL
+#TODO remove a file on HAL
+#TODO add support for multiple files
+
 ##curl -v -u bthom:dissemin api-preprod.archives-ouvertes.fr/sword/hal -H "X-Packaging:http://purl.org/net/sword-types/AOfr" -X POST -H "Content-Type:text/xml" -d @art.xml
+
