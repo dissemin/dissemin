9dbedd35e648e64cd895b67f9e6557eff21b75ba
Antonin Delpeuch <antonin@delpeuch.eu>
2015-03-21 10:17:12 +0100

    Add 'Host' header for more explicit errors

--- sword/submitPaper.py
+++ sword/submitPaper.py
@@ -22,75 +22,77 @@ conn = httplib.HTTPConnection("api-preprod.archives-ouvertes.fr")
 
 #TODO Abstract on the connection
 def CreateZipFromPdfAndMetadata(pdf,metadata):
-	s = StringIO.StringIO()
-	with ZipFile(s,'w') as myZip:
-		myZip.writestr("article.pdf",pdf) 
-		myZip.writestr("meta.xml",metadata)
-		myZip.close()
-		return s.getvalue()
+    s = StringIO.StringIO()
+    with ZipFile(s,'w') as myZip:
+        myZip.writestr("article.pdf",pdf) 
+        myZip.writestr("meta.xml",metadata)
+        myZip.close()
+        return s.getvalue()
 
 
 
 def MetadataHal(strData):  #Homemade sword protocol 
 #TODO refactorize the code, fold on the headers
-	conn.putrequest("POST", "/sword/hal/", True,True)
-	conn.putheader("Authorization",encodeUserData(u,p))
-	conn.putheader("Host","api-preprod.archives-ouvertes.fr")
-	conn.putheader("X-Packaging","http://purl.org/net/sword-types/AOfr")
-	conn.putheader("Content-Type","text/xml")
-	conn.putheader("Content-Length",len(strData))
-	conn.endheaders()
-	conn.send(strData)
-	r1 = conn.getresponse()
-	print r1.read()
+    conn.putrequest("POST", "/sword/hal/", True,True)
+    conn.putheader("Authorization",encodeUserData(u,p))
+    conn.putheader("Host","api-preprod.archives-ouvertes.fr")
+    conn.putheader("X-Packaging","http://purl.org/net/sword-types/AOfr")
+    conn.putheader("Content-Type","text/xml")
+    conn.putheader("Content-Length",len(strData))
+    conn.endheaders()
+    conn.send(strData)
+    r1 = conn.getresponse()
+    print r1.read()
 
 
 
 
 def FullHal(pdf,metadata):
-	strData = CreateZipFromPdfAndMetadata(pdf,metadata)
-	conn.putrequest("POST", "/sword/hal/", True,True)
-	conn.putheader("Authorization",encodeUserData(u,p))
-	conn.putheader("User-Agent", "curl/7.35.0")
-	conn.putheader("Host","api-preprod.archives-ouvertes.fr")
-	conn.putheader("Packaging","http://purl.org/net/sword-types/AOfr")
-	conn.putheader("Content-Type","application/zip")
-	conn.putheader("Content-Disposition"," attachment; filename=meta.xml")
-	conn.putheader("Content-Length",len(strData))
-	conn.endheaders()
-	conn.send(strData)
-	r1 = conn.getresponse()
-	print r1.read()
+    strData = CreateZipFromPdfAndMetadata(pdf,metadata)
+    conn.putrequest("POST", "/sword/hal/", True,True)
+    conn.putheader("Authorization",encodeUserData(u,p))
+    conn.putheader("User-Agent", "curl/7.35.0")
+    conn.putheader("Host","api-preprod.archives-ouvertes.fr")
+    conn.putheader("Packaging","http://purl.org/net/sword-types/AOfr")
+    conn.putheader("Content-Type","application/zip")
+    conn.putheader("Content-Disposition"," attachment; filename=meta.xml")
+    conn.putheader("Content-Length",len(strData))
+    conn.endheaders()
+    conn.send(strData)
+    r1 = conn.getresponse()
+    print r1.read()
 #WARNING, ElementTree is not secure against malicious constructed data.
 #I will assume that the online "edition" is where I want
 
 def UpdateHal(pdf,idHal):
-	with open("sword/t/deposit.zip","r") as zp:	
-		#metadata = mt.read()
-#		metadata = etree.fromstring(mt.read())
-#		nodeInteresting = metadata.find(".//{http://www.tei-c.org/ns/1.0}edition")
-#		etree.SubElement(nodeInteresting, 'ref', type="file", target="article.pdf")  	
-#		metadata = etree.tostring(metadata)
-#		print metadata 
-		strData = zp.read()#CreateZipFromPdfAndMetadata(pdf,metadata)
-		conn.putrequest("PUT", "https://api-preprod.archives-ouvertes.fr/sword/"+idHal, True,True)
-		conn.putheader("Authorization",encodeUserData(u,p))
-		conn.putheader("Packaging","http://purl.org/net/sword-types/AOfr")
-		conn.putheader("Content-Type","application/zip")
-		conn.putheader("Content-Disposition"," attachment; filename=meta.xml")
-		conn.putheader("Content-Length",len(strData))
-		conn.endheaders()
-		print "Sending... :\n"
-		conn.send(strData)
-		print "Sent :\n"
-		r1 = conn.getresponse()
-		print "Response :\n"
-		print r1.read()
+    with open("sword/t/deposit.zip","r") as zp:    
+        #metadata = mt.read()
+#        metadata = etree.fromstring(mt.read())
+#        nodeInteresting = metadata.find(".//{http://www.tei-c.org/ns/1.0}edition")
+#        etree.SubElement(nodeInteresting, 'ref', type="file", target="article.pdf")      
+#        metadata = etree.tostring(metadata)
+#        print metadata 
+        strData = zp.read()#CreateZipFromPdfAndMetadata(pdf,metadata)
+        conn.putrequest("PUT", "/sword/"+idHal, True,True)
+        conn.putheader("Host", "api-preprod.archives-ouvertes.fr")
+        conn.putheader("Authorization",encodeUserData(u,p))
+        conn.putheader("Packaging","http://purl.org/net/sword-types/AOfr")
+        conn.putheader("Content-Type","application/zip")
+        conn.putheader("Content-Disposition"," attachment; filename=meta.xml")
+        conn.putheader("Content-Length",len(strData))
+#        conn.putheader("Metadata-Relevant", "False")
+        conn.endheaders()
+        print "Sending... :\n"
+        conn.send(strData)
+        print "Sent :\n"
+        r1 = conn.getresponse()
+        print "Response :\n"
+        print r1.read()
 
 # edition editionStmt biblFull  listBibl body text tei
 def bla():
-	with open("sword/t/article.pdf","r") as art:
- 		UpdateHal(art.read(),"lirmm-00090366")
+    with open("sword/t/article.pdf","r") as art:
+         UpdateHal(art.read(),"lirmm-00090366")
 
 #TODO parse the XML to search "<edition>" then add the src
 #TODO Full SSL. Right now user/pass are not crypted.
