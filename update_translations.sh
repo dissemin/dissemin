0b42dfbaae445fdfe25f1b4719f483517aed08cc
Antonin Delpeuch <antonin@delpeuch.eu>
2015-09-23 12:24:14 +0200

    Attempt to update translations from travis

--- update_translations.sh
+++ update_translations.sh
@@ -2,11 +2,15 @@
 
 # update translations, report missing and fuzzy translations
 
-# go into directory of script
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-cd "$DIR"
+if [ -z "$TRAVIS" ]; then
+    echo "CD"
+    # go into directory of script
+    DIR="$( cd "$( dirname "$0" )" && pwd )"
+    cd "$DIR"
 
-git pull -q
+    git pull -q
+fi
+
 python manage.py makemessages -l fr >&2
 msggrep -v -T -e '.' locale/fr/LC_MESSAGES/django.po
 awk -v RS='' '/#, fuzzy/' locale/fr/LC_MESSAGES/django.po
