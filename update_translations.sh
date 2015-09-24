8fb21744c815a5f58cc34c0aec16a6b7299f5e0c
Antoine Amarilli <a3nm@a3nm.net>
2015-09-24 11:31:24 +0200

    ignore fuzzy translations when commented out

--- update_translations.sh
+++ update_translations.sh
@@ -12,5 +12,8 @@ fi
 
 python manage.py makemessages -l fr >&2
 msggrep -v -T -e '.' locale/fr/LC_MESSAGES/django.po
-awk -v RS='' '/#, fuzzy/' locale/fr/LC_MESSAGES/django.po
+# fuzzy translations are in a paragraph containing #, fuzzy
+# but we must exclude those where msgstr is itself commented out
+# (it is at BOL when not commented out, and preceded by a space otherwise)
+awk -v RS='' '/#, fuzzy.*[^ ]msgstr/' locale/fr/LC_MESSAGES/django.po
 
