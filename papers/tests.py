a9208956b31b87676eba7c55af240c5f26f84ebd
Antonin Delpeuch <antonin@delpeuch.eu>
2015-12-03 17:53:21 +0100

    New tests on paper merge

--- papers/tests.py
+++ papers/tests.py
@@ -22,6 +22,7 @@ from __future__ import unicode_literals
 
 import unittest
 import django.test
+from datetime import date
 from papers.models import *
 import papers.doi
 import datetime
@@ -58,5 +59,45 @@ import doctest
 def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite(papers.doi))
     return tests
+
+
+class PaperTest(django.test.TestCase):
+    def test_create_by_doi(self):
+        p = Paper.create_by_doi('10.1109/synasc.2010.88')
+        self.assertEqual(p.title, 'Monitoring and Support of Unreliable Services')
+
+    def test_create_no_authors(self):
+        p = Paper.create_by_doi('10.1021/cen-v043n050.p033')
+        self.assertEqual(p, None)
+
+    def test_create_invalid_doi(self):
+        p = Paper.create_by_doi('10.1021/eiaeuiebop134223cen-v043n050.p033')
+        self.assertEqual(p, None)
+
+    def test_merge(self):
+        # Get a paper with CrossRef metadata
+        p = Paper.create_by_doi('10.1111/j.1744-6570.1953.tb01038.x')
+        p.save()
+        ccf = get_ccf()
+        # Create a copy with slight variations
+        names = map(Name.lookup_name, [('M. H.','Jones'),('R. H.', 'Haase'),('S. F.','Hulbert')])
+        p2 = ccf.get_or_create_paper('A Survey of the Literature on Technical Positions', names,
+                date(year=2011, month=01, day=01))
+        # The two are not merged because of the difference in the title
+        self.assertNotEqual(p, p2)
+        # Fix the title of the second one
+        p2.title = 'A Survey of the Literature on Job Analysis of Technical Positions'
+        p2.save()
+        # Check that the new fingerprint is equal to that of the first paper
+        self.assertEqual(p2.new_fingerprint(), p.fingerprint)
+        # and that the new fingerprint and the current differ
+        self.assertNotEqual(p2.new_fingerprint(), p2.fingerprint)
+        # and that the first paper matches its own shit
+        self.assertEqual(Paper.objects.filter(fingerprint=p.fingerprint).first(), p)
+        # The two papers should hence be merged together
+        new_paper = p2.recompute_fingerprint_and_merge_if_needed()
+        self.assertEqual(new_paper.pk, p.pk)
+
+
 
 
