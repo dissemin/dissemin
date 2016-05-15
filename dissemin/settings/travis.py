cf75d40d2eafd9da56e15b59b5d94054bb67e8d6
Raito Bezarius <masterancpp@gmail.com>
2016-05-15 17:48:08 +0200

    dissemin(settings,ci): attempt to fix again Travis settings

--- dissemin/settings/travis.py
+++ dissemin/settings/travis.py
@@ -3,6 +3,26 @@ Travis specific settings for tests
 """
 
 from .common import *
+import os
+
+if 'CORE_API_KEY' in os.environ:
+    CORE_API_KEY = os.environ['CORE_API_KEY']
+
+if 'ROMEO_API_KEY' in os.environ:
+    ROMEO_API_KEY = os.environ['ROMEO_API_KEY']
+
+# Travis settings
+# taken from https://gist.github.com/ndarville/3625246
+DATABASES = {
+    'default': {
+        'ENGINE':   'django.db.backends.postgresql_psycopg2',
+        'NAME':     'dissemin',
+        'USER':     'postgres',
+        'PASSWORD': '',
+        'HOST':     'localhost',
+        'PORT':     '',
+    }
+}
 
 # Patch urllib3 because the default SSL module on Travis sucks
 import urllib3.contrib.pyopenssl
