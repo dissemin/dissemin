aa96575b7eff26eb640cc91c26c44e243c206a45
Raito Bezarius <masterancpp@gmail.com>
2016-05-16 00:49:46 +0200

    notification(jsonfield): use a proper jsonfield...

---
+++ notification/migrations/0003_auto_20160516_0045.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import jsonfield.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('notification', '0002_notification_tag'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='notification',
+            name='payload',
+            field=jsonfield.fields.JSONField(),
+        ),
+    ]
