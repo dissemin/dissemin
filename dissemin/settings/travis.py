301431c85891efb3f890bfb1cbb3c6851a71759c
Raito Bezarius <masterancpp@gmail.com>
2016-05-15 23:58:17 +0200

    env(travis): add missing settings

--- dissemin/settings/travis.py
+++ dissemin/settings/travis.py
@@ -5,6 +5,14 @@ Travis specific settings for tests
 from .common import *
 import os
 
+# Cache backend
+# https://docs.djangoproject.com/en/1.8/topics/cache/
+CACHES = {
+    'default': {
+	'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+    }
+}
+
 if 'CORE_API_KEY' in os.environ:
     CORE_API_KEY = os.environ['CORE_API_KEY']
 
@@ -26,6 +34,23 @@ DATABASES = {
         'PORT':     '',
     }
 }
+
+# Base domain of the ORCiD API.
+ORCID_BASE_DOMAIN = 'sandbox.orcid.org' # for the sandbox API
+
+# ORCiD provider configuration (sandbox)
+SOCIALACCOUNT_PROVIDERS = \
+   {'orcid':
+       {
+        'BASE_DOMAIN': ORCID_BASE_DOMAIN,
+         # Member API or Public API? Default: False (for the public API)
+         'MEMBER_API': False, # for the member API
+       }
+   }
+
+
+# Debug Email Backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Patch urllib3 because the default SSL module on Travis sucks
 import urllib3.contrib.pyopenssl
