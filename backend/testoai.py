1a3b8dee1e14e3ea6ba1bba563098a6325c5db4b
Antonin Delpeuch <antonin@delpeuch.eu>
2016-06-15 17:13:38 +0100

    Fixing tests

--- backend/testoai.py
+++ backend/testoai.py
@@ -58,7 +58,13 @@ class OaiTest(TestCase):
         Creation of a paper from an OAI record,
         when the paper does not exist yet.
         """
-        pass
+        oai_id ='ftccsdartic:oai:hal.archives-ouvertes.fr:hal-00830421'
+
+        # first, make sure the paper isn't there already
+        self.delete(oai_id)
+        # create a paper from BASE
+        hal_paper = self.create(oai_id, 'base_dc')
+        self.assertEqual(len(hal_paper.oairecords), 1)
     
     def test_create_already_existing(self):
         """
@@ -78,8 +84,9 @@ class OaiTest(TestCase):
 
         # It's the same thing!
         self.assertEqual(new_paper, hal_paper)
-        self.assertEqual(new_paper.oairecords, hal_paper.oairecords)
-        self.assertEqual(new_paper.authors, hal_paper.authors)
+        self.assertSetEqual(set(new_paper.oairecords), set(hal_paper.oairecords))
+        self.assertListEqual(new_paper.bare_author_names(),
+                            hal_paper.bare_author_names())
         self.assertEqual(new_paper.title, hal_paper.title)
  
     
@@ -108,7 +115,7 @@ class OaiTest(TestCase):
         # the new set of records is the old one plus the new record
         records.add(OaiRecord.objects.get(identifier=
                 'ftciteseerx:oai:CiteSeerX.psu:10.1.1.487.869'))
-        self.assertEqual(set(new_paper.oairecords), records)
+        self.assertSetEqual(set(new_paper.oairecords), records)
 
     @unittest.expectedFailure
     def test_create_match_doi(self):
@@ -128,18 +135,31 @@ class OaiTest(TestCase):
             'oai:crossref.org:10.1007/s10858-015-9994-8',
             'citeproc')
         self.assertEqual(spoc, new_paper)
-        print new_paper.oairecords
         records.add(OaiRecord.objects.get(identifier=
             'oai:crossref.org:10.1007/s10858-015-9994-8'))
         self.assertEqual(set(new_paper.oairecords), records)
 
+    @unittest.expectedFailure
     def test_create_match_identifier(self):
         """
         An OAI record with the same identifier already
         exists but it has been already merged before with
         another paper with a different fingerprint.
         """
-        pass
+        identifier = 'ftccsdartic:oai:hal.archives-ouvertes.fr:hal-00939473'
+
+        # create a paper from BASE
+        hal_paper = self.create(identifier, 'base_dc')
+        records = set(hal_paper.oairecords)
+        # change its fingerprint
+        hal_paper.title += ' babebibobu'
+        hal_paper.fingerprint = hal_paper.new_fingerprint()
+        hal_paper.save()
+        
+        # add the OAI record again
+        new_paper = self.create(identifier, 'base_dc')
+        self.assertEqual(new_paper, hal_paper)
+        self.assertSetEqual(records, set(new_paper.oairecords))
 
     def test_create_invalid_metadata(self):
         """
