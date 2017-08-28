6af57f77a9a32e6977eff5f866b29d2db11f34d9
Antonin Delpeuch <antonin@delpeuch.eu>
2017-08-28 15:36:55 +0200

    Add missing models.py file >_<

---
+++ deposit/osf/models.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from deposit.models import DepositPreferences
+
+class OSFDepositPreferences(DepositPreferences):
+    on_behalf_of = models.CharField(max_length=128,
+                null=True,
+                blank=True,
+                verbose_name=_('OSF identifier'),
+                help_text=_('If set, your deposits will be associated to this OSF account. This identifier is a string of letters and numbers which can be found in your OSF public profile URL.'))
+
+
