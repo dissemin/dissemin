395b411f31fc260bd9838ac3f203f7cf8073bec1
Stefan Beck <stefan.beck@ulb.tu-darmstadt.de>
2019-05-23 08:59:27 +0200

    Admin views shall not show translated values

--- dissemin/settings/common.py
+++ dissemin/settings/common.py
@@ -175,6 +175,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'vinaigrette.middleware.VinaigretteAdminLanguageMiddleware',
 ]
 
 AUTHENTICATION_BACKENDS = (
