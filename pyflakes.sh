1596e79c4b233a92c2e5b46bf5e511ef44a03535
Stefan Beck <stefan.beck@ulb.tu-darmstadt.de>
2019-07-22 10:06:24 +0200

    Use pyflakes3 instead of pyflakes

--- pyflakes.sh
+++ pyflakes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-find -maxdepth 1 -type d | grep -vP "^.($|/dissemin|/notification|/src|/\..*)" | xargs pyflakes > pyflakes.out
+find -maxdepth 1 -type d | grep -vP "^.($|/dissemin|/notification|/src|/\..*)" | xargs pyflakes3 > pyflakes.out
 cat pyflakes.out
 test \! -s pyflakes.out
