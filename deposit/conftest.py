c323c12f53395eb057b03c5f56fb896fbe03a895
Stefan Beck <stefan.beck@ulb.tu-darmstadt.de>
2019-08-22 12:06:58 +0200

    Fixtures were unordered

--- deposit/conftest.py
+++ deposit/conftest.py
@@ -11,90 +11,6 @@ from papers.models import Paper
 from papers.models import Researcher
 
 
-@pytest.fixture
-def empty_user_preferences(db, user_isaac_newton):
-    """
-    Returns an empty UserPreferences object
-    """
-    user_prefs, unused = UserPreferences.objects.get_or_create(user=user_isaac_newton)
-    return user_prefs
-
-
-@pytest.fixture()
-def license_standard(db):
-    """
-    Returns a standard test license
-    """
-
-    license = License.objects.get_or_create(
-        name="Standard License",
-        uri="https://dissem.in/deposit/license/standard"
-    )
-
-    return license
-
-
-@pytest.fixture()
-def license_alternative(db):
-    """
-    Returns an alternative test license
-    Use this license, if you need two of them
-    """
-
-    license = License.objects.get_or_create(
-        name="Alternative License",
-        uri="https://dissem.in/deposit/license/alternative"
-    )
-
-    return license
-
-
-# This is a list of oairecords to be tested
-metadata_publications = [
-    'book-chapter_acute_interstitial_nephritis',
-    'book-chapter_adaptive_multiagent_system_for_multisensor_maritime_surveillance',
-    'book_god_of_the_labyrinth',
-    'dataset_sexuality_assessment_in_older_adults_unique_situations',
-    'journal-article_a_female_signal_reflects_mhc_genotype_in_a_social_primate',
-    'journal-article_altes_und_neues_zum_strafrechtlichen_vorsatzbegriff',
-    'journal-article_confrontation_with_heidegger',
-    'journal-article_constructing_matrix_geometric_means',
-    'journal-article_lobjet_de_la_dpression',
-    'journal-issue_mode_und_gender',
-    'other_assisting_configuration_based_feature_model_composition',
-    'poster_development_of_new_gpe_conjugates_with_application_in_neurodegenerative_diseases',
-    'preprint_nikomachische_ethik',
-    'proceedings_sein_und_nichtsein',
-    'proceedings-article_activitycentric_support_for_weaklystructured_business_processes',
-    'proceedings-article_an_efficient_vofbased_rans_method_to_capture_complex_sea_states',
-    'proceedings-article_cursos_efa',
-    'reference-entry_chromatin_interaction_analysis_using_pairedend_tag_sequencing',
-    'report_execution_of_targeted_experiments_to_inform_bison',
-    'thesis_blue_mining_planung_des_abbaus_von_manganknollen_in_der_tiefsee',
-]
-
-
-@pytest.fixture(params=metadata_publications)
-def upload_data(request, load_json):
-    """
-    Loads the above list of publications and returns form data the user has to fill in.
-    """
-    return load_json.load_upload(request.param)
-
-
-@pytest.fixture(params=[True, False])
-def ddc(request, db):
-    """
-    Run tests for protocol with repository having a DDC and not having a DDC
-    """
-    if request.param:
-        all_ddc = DDC.objects.all()
-        request.cls.protocol.repository.ddc.add(*all_ddc)
-        return all_ddc
-    else:
-        return None
-
-
 @pytest.fixture(params=[None, '2543-2454-2345-234X'])
 def depositing_user(db, request, user_leibniz):
     """
@@ -108,6 +24,19 @@ def depositing_user(db, request, user_leibniz):
     )
 
     return user_leibniz
+
+
+@pytest.fixture(params=[True, False])
+def ddc(request, db):
+    """
+    Run tests for protocol with repository having a DDC and not having a DDC
+    """
+    if request.param:
+        all_ddc = DDC.objects.all()
+        request.cls.protocol.repository.ddc.add(*all_ddc)
+        return all_ddc
+    else:
+        return None
 
 
 @pytest.fixture
@@ -129,6 +58,44 @@ def dissemin_xsd_1_0():
     testdir = os.path.dirname(os.path.abspath(__file__))
     dissemin_xsd = etree.parse(os.path.join(testdir, 'schema','dissemin_v1.0.xsd'))
     return etree.XMLSchema(dissemin_xsd)
+
+
+@pytest.fixture
+def empty_user_preferences(db, user_isaac_newton):
+    """
+    Returns an empty UserPreferences object
+    """
+    user_prefs, unused = UserPreferences.objects.get_or_create(user=user_isaac_newton)
+    return user_prefs
+
+
+@pytest.fixture()
+def license_alternative(db):
+    """
+    Returns an alternative test license
+    Use this license, if you need two of them
+    """
+
+    license = License.objects.get_or_create(
+        name="Alternative License",
+        uri="https://dissem.in/deposit/license/alternative"
+    )
+
+    return license
+
+
+@pytest.fixture()
+def license_standard(db):
+    """
+    Returns a standard test license
+    """
+
+    license = License.objects.get_or_create(
+        name="Standard License",
+        uri="https://dissem.in/deposit/license/standard"
+    )
+
+    return license
 
 
 @pytest.fixture(params=[True, False])
@@ -167,3 +134,36 @@ def request_fake_response():
     r.url = 'https://dissem.in'
 
     return r
+
+
+# This is a list of oairecords to be tested
+metadata_publications = [
+    'book-chapter_acute_interstitial_nephritis',
+    'book-chapter_adaptive_multiagent_system_for_multisensor_maritime_surveillance',
+    'book_god_of_the_labyrinth',
+    'dataset_sexuality_assessment_in_older_adults_unique_situations',
+    'journal-article_a_female_signal_reflects_mhc_genotype_in_a_social_primate',
+    'journal-article_altes_und_neues_zum_strafrechtlichen_vorsatzbegriff',
+    'journal-article_confrontation_with_heidegger',
+    'journal-article_constructing_matrix_geometric_means',
+    'journal-article_lobjet_de_la_dpression',
+    'journal-issue_mode_und_gender',
+    'other_assisting_configuration_based_feature_model_composition',
+    'poster_development_of_new_gpe_conjugates_with_application_in_neurodegenerative_diseases',
+    'preprint_nikomachische_ethik',
+    'proceedings_sein_und_nichtsein',
+    'proceedings-article_activitycentric_support_for_weaklystructured_business_processes',
+    'proceedings-article_an_efficient_vofbased_rans_method_to_capture_complex_sea_states',
+    'proceedings-article_cursos_efa',
+    'reference-entry_chromatin_interaction_analysis_using_pairedend_tag_sequencing',
+    'report_execution_of_targeted_experiments_to_inform_bison',
+    'thesis_blue_mining_planung_des_abbaus_von_manganknollen_in_der_tiefsee',
+]
+
+
+@pytest.fixture(params=metadata_publications)
+def upload_data(request, load_json):
+    """
+    Loads the above list of publications and returns form data the user has to fill in.
+    """
+    return load_json.load_upload(request.param)
