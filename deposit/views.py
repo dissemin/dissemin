dissemin/dissemin

commit 94f57c89f1ab5654766d64bc594c91c981813933

Make sure, that the preferred or last repository obtained is enabled

deposit/views.py
@@ -81,31 +81,20 @@ def get_metadata_form(request):
 def start_view(request, pk):
     paper = get_object_or_404(Paper, pk=pk)
     repositories = get_all_repositories_and_protocols(paper, request.user)
-    repositories_protocol = {repo.id :proto for repo, proto in repositories}
-
+    
     # select the most appropriate repository
-    selected_repository = None
-    selected_protocol = None
-
-    # Try to get the preferred one
     userprefs = UserPreferences.get_by_user(request.user)
-    if userprefs.preferred_repository:
-        selected_repository = userprefs.preferred_repository
-        selected_protocol = repositories_protocol.get(selected_repository.id)
-
-    # Try to get the last one used by this user
-    logger.info(userprefs.last_repository)
-    if selected_protocol is None and userprefs.last_repository:
-        selected_repository = userprefs.last_repository
-        selected_protocol = repositories_protocol.get(selected_repository.id)
-
-    # If none of these are available, pick any
-    if selected_protocol is None:
+    preselected_repository = userprefs.get_preferred_or_last_repository()
+    preselected_protocol = None
+    if not preselected_repository:
         for repo, protocol in repositories:
             if protocol is not None:
-                selected_repository = repo
-                selected_protocol = protocol
+                preselected_repository = repo
+                preselected_protocol = protocol
                 break
+    else:
+        repositories_protocol = {repo.id :proto for repo, proto in repositories}
+        preselected_protocol = repositories_protocol.get(preselected_repository.id)
 
     breadcrumbs = paper.breadcrumbs()
     breadcrumbs.append((_('Deposit'), ''))
@@ -113,8 +102,8 @@ def start_view(request, pk):
             'paper': paper,
             'max_file_size': settings.DEPOSIT_MAX_FILE_SIZE,
             'available_repositories': repositories,
-            'selected_repository': selected_repository,
-            'selected_protocol': selected_protocol,
+            'selected_repository': preselected_repository,
+            'selected_protocol': preselected_protocol,
             'is_owner': paper.is_owned_by(request.user, flexible=True),
             'breadcrumbs': breadcrumbs,
             'repositoryForm': None,

deposit/models.py
@@ -244,6 +244,7 @@ class UserPreferences(models.Model):
     def get_last_repository(self):
         """
         Use this function to get the last repository of a user. This function silently sets the ``last_repository`` to ``None`` if it is not enabled.
+        :return: Repository instance or ``None``
         """
         if self.last_repository and not self.last_repository.enabled:
             self.last_repository = None
@@ -254,10 +255,20 @@ class UserPreferences(models.Model):
         """
         Use this function to get the preferred repository of a user.
         This function silently sets ``preferred_repository`` to ``None`` if it is currently not enabled.
-        :return: Repository instance or None
+        :return: Repository instance or ``None``
         """
         if self.preferred_repository and not self.preferred_repository.enabled:
             self.preferred_repository = None
             self.save()
         return self.preferred_repository
+
+    def get_preferred_or_last_repository(self):
+        """
+        Use this function to get the preferred or last repository of a user.
+        :return: Repository instance or ``None``
+        """
+        repo = self.get_preferred_repository()
+        if not repo:
+            repo = self.get_last_repository()
+        return repo 
 

deposit/tests/conftest.py
@@ -11,3 +11,9 @@ def empty_user_preferences(db, user_isaac_newton):
     user_prefs, unused = UserPreferences.objects.get_or_create(user=user_isaac_newton)
     yield user_prefs
     user_prefs.delete()
+
+@pytest.fixture(params=[True, False])
+def dummy_repository(request, dummy_repository):
+    dummy_repository.enabled = request.param
+    dummy_repository.save()
+    return dummy_repository

deposit/tests/test_user_preferences.py
@@ -3,27 +3,17 @@ class TestUserPreferences():
     Groups tests for model UserPreferences
     """
 
-    def test_get_last_repository_enabled(self, dummy_repository, empty_user_preferences):
+    def test_get_last_repository(self, dummy_repository, empty_user_preferences):
         """
-        If last repository is enabled, return repository
-        """
-        empty_user_preferences.last_repository = dummy_repository
-        empty_user_preferences.save()
-        
-        assert empty_user_preferences.get_last_repository() == dummy_repository
-
-
-    def test_get_lastrepository_disabled(self, dummy_repository, empty_user_preferences):
-        """
-        If last repository is disabled, return None and set last repository to None
+        If last repository is enabled, return repository else return None and set default repository to None
         """
         empty_user_preferences.last_repository = dummy_repository
         empty_user_preferences.save()
-        dummy_repository.enabled = False
-        dummy_repository.save()
-
-        assert empty_user_preferences.get_last_repository() == None
-        assert empty_user_preferences.last_repository == None
+        if dummy_repository.enabled:
+            assert empty_user_preferences.get_last_repository() == dummy_repository
+        else:
+            assert empty_user_preferences.get_last_repository() == None
+            assert empty_user_preferences.last_repository == None
 
     def test_get_last_repository_none(self, empty_user_preferences):
         """
@@ -31,30 +21,39 @@ class TestUserPreferences():
         """
         assert empty_user_preferences.get_last_repository() == None
 
-    def test_get_preferred_repository_enabled(self, dummy_repository, empty_user_preferences):
+    def test_get_preferred_repository(self, dummy_repository, empty_user_preferences):
         """
-        If preferred repository is enabled, return repository
-        """
-        empty_user_preferences.preferred_repository = dummy_repository
-        empty_user_preferences.save()
-        
-        assert empty_user_preferences.get_preferred_repository() == dummy_repository
-
-    def test_get_preferred_repository_disabled(self, dummy_repository, empty_user_preferences):
-        """
-        If preferred repository is disabled, return None and set default repository to None
+        If preferred repository is enabled, return repository else return None and set default repository to None
         """
         empty_user_preferences.preferred_repository = dummy_repository
         empty_user_preferences.save()
-        dummy_repository.enabled = False
-        dummy_repository.save()
+        if dummy_repository.enabled:
+            assert empty_user_preferences.get_preferred_repository() == dummy_repository
+        else:
+            assert empty_user_preferences.get_preferred_repository() == None
+            assert empty_user_preferences.preferred_repository == None
 
-        assert empty_user_preferences.get_preferred_repository() == None
-        assert empty_user_preferences.preferred_repository == None
 
     def test_get_preferred_repository_none(self, empty_user_preferences):
         """
         If no preferred repository is set, return None
         """
         assert empty_user_preferences.get_preferred_repository() == None
+
+    def test_get_preferred_or_last_repository_preferred(self, dummy_repository, empty_user_preferences):
+        """
+        If preferred repository is set, return this or None
+        """
+        empty_user_preferences.preferred_repository = dummy_repository
+        empty_user_preferences.save()
+        assert empty_user_preferences.get_preferred_or_last_repository() == empty_user_preferences.get_preferred_repository()
+
+    def test_get_preferred_or_last_repository_last(self, dummy_repository, empty_user_preferences):
+        """
+        If preferred is None or not enabled, return last repository or None
+        """
+        empty_user_preferences.last_repository = dummy_repository
+        empty_user_preferences.save()
+        assert empty_user_preferences.get_preferred_or_last_repository() == empty_user_preferences.get_last_repository()
+
 

commit f8fe581e4b90033135d9a07aefd9eeb27984edc7

Fix repository selection on deposit page.
This bug was introduced by 94f57c89f.
Closes #824.

deposit/views.py
@@ -83,20 +83,21 @@ def get_metadata_form(request):
 def start_view(request, pk):
     paper = get_object_or_404(Paper, pk=pk)
     repositories = get_all_repositories_and_protocols(paper, request.user)
+    repositories_protocol = {repo.id :proto for repo, proto in repositories}
     
     # select the most appropriate repository
     userprefs = UserPreferences.get_by_user(request.user)
     preselected_repository = userprefs.get_preferred_or_last_repository()
     preselected_protocol = None
-    if not preselected_repository:
+    if preselected_repository:
+        preselected_protocol = repositories_protocol.get(preselected_repository.id)
+    # If the preferred repository is not available for this paper, pick any
+    if not preselected_protocol:
         for repo, protocol in repositories:
             if protocol is not None:
                 preselected_repository = repo
                 preselected_protocol = protocol
                 break
-    else:
-        repositories_protocol = {repo.id :proto for repo, proto in repositories}
-        preselected_protocol = repositories_protocol.get(preselected_repository.id)
 
     breadcrumbs = paper.breadcrumbs()
     breadcrumbs.append((_('Deposit'), ''))