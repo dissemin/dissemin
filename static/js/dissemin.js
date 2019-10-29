72b525d0b6c084dc9bfde52b6833ed3f25fd9015
Stefan Beck <stefan.beck@ulb.tu-darmstadt.de>
2019-10-29 11:37:59 +0100

    Add JS-function to get csrf token

--- static/js/dissemin.js
+++ static/js/dissemin.js
@@ -3,6 +3,28 @@
 
 
 /* ***
+ * CSRF
+ * *** */
+
+/* Returns the current csrf token from the cookie. This is the recommend method by django: https://docs.djangoproject.com/en/2.2/ref/csrf/#ajax */
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+
+/* ***
  * Navigation
  * *** */
 
