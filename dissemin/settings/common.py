c4448f314b3118181951193dab81a08d7248dadd
Stefan Beck <stefan.beck@ulb.tu-darmstadt.de>
2019-11-19 08:53:35 +0100

    Error: The STATICFILES_DIRS setting should not contain the STATIC_ROOT setting.

--- dissemin/settings/common.py
+++ dissemin/settings/common.py
@@ -221,8 +221,6 @@ WSGI_APPLICATION = 'dissemin.wsgi.application'
 # This defines how static files are stored and accessed.
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 #
-# We add /static as place to look for static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'dissemin', 'static'),]
 # Relative URL where static files are accessed (you don't have to change this).
 STATIC_URL = '/static/'
 # Relative URL where user uploads are accessed (you don't have to change this).
