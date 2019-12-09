a168fb92364c8f1576eb115530e6916721abc5fc
Stefan Beck <stefan.beck@ulb.tu-darmstadt.de>
2019-12-09 16:55:33 +0100

    imports unordered

--- backend/tests/test_utils.py
+++ backend/tests/test_utils.py
@@ -1,9 +1,8 @@
-
+from datetime import timedelta
+from time import sleep
 
 from backend.utils import report_speed
 from backend.utils import with_speed_report
-from time import sleep
-from datetime import timedelta
 
 def test_report_speed():
     assert list(with_speed_report(range(10))) == list(range(10))
@@ -16,4 +15,3 @@ def test_report_speed():
             yield elem
     
     assert list(second_generator(20)) == list(range(20))
-    
