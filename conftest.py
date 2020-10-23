cb717e56006aa924dcb013a41b0b75cd8d8efce7
Stefan Beck <stefan.beck@ulb.tu-darmstadt.de>
2020-10-23 08:40:00 +0200

    Switch to pytest-django-4 and use newer pytest
    Due to some mixture of pytest and Unittest, we encoutered some obscure errors with newer versions of pytest. With excluding ProtocolTest that does partial testing of protocols this is fixed.
    
    ProtocolTest uses some pytest-fixtures and, when gets invoked multiple
    times, does no proper teardownn of the fixture. As a result, the
    database is not clean after the ProtocolTest and this can mess up some
    of the tests - if they rely on an empty database.
    
    Downside is that we currently have no Zenodo tests, but I think this is
    minor important. For one, these tests fail sometimes due to Zenodo
    hickups and they invoked some dry-run, so they were not testing the real
    thing.

--- conftest.py
+++ conftest.py
@@ -727,23 +727,22 @@ def get_researcher_by_name(first, last):
 
 
 @pytest.fixture
-def load_test_data(request, db, django_db_setup, django_db_blocker):
-    with django_db_blocker.unblock():
-        call_command('loaddata', 'test_dump.json')
-        self = request.cls
-        self.i = Institution.objects.get(name='ENS')
-        self.d = Department.objects.get(name='Chemistry dept')
-        self.di = Department.objects.get(name='Comp sci dept')
+def load_test_data(request, db):
+    call_command('loaddata', 'test_dump.json')
+    self = request.cls
+    self.i = Institution.objects.get(name='ENS')
+    self.d = Department.objects.get(name='Chemistry dept')
+    self.di = Department.objects.get(name='Comp sci dept')
 
-        self.r1 = get_researcher_by_name('Isabelle', 'Aujard')
-        self.r2 = get_researcher_by_name('Ludovic', 'Jullien')
-        self.r3 = get_researcher_by_name('Antoine', 'Amarilli')
-        self.r4 = get_researcher_by_name('Antonin', 'Delpeuch')
-        self.r5 = get_researcher_by_name('Terence', 'Tao')
-        self.hal = OaiSource.objects.get(identifier='hal')
-        self.arxiv = OaiSource.objects.get(identifier='arxiv')
-        self.lncs = Journal.objects.get(issn='0302-9743')
-        self.acm = Journal.objects.get(issn='1529-3785').publisher
+    self.r1 = get_researcher_by_name('Isabelle', 'Aujard')
+    self.r2 = get_researcher_by_name('Ludovic', 'Jullien')
+    self.r3 = get_researcher_by_name('Antoine', 'Amarilli')
+    self.r4 = get_researcher_by_name('Antonin', 'Delpeuch')
+    self.r5 = get_researcher_by_name('Terence', 'Tao')
+    self.hal = OaiSource.objects.get(identifier='hal')
+    self.arxiv = OaiSource.objects.get(identifier='arxiv')
+    self.lncs = Journal.objects.get(issn='0302-9743')
+    self.acm = Journal.objects.get(issn='1529-3785').publisher
 
 
 @pytest.fixture
